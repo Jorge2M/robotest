1f8dae670cf833c852c44349df182eb6e60587b4
Jorge Munoz <jorge.munoz.sge@mango.com>
2019-01-21 17:46:46 +0100

    SYS-3861

---
+++ webmngtest/src/main/java/org/pruebasws/utils/TSuiteThreadsManager.java
@@ -0,0 +1,88 @@
+package org.pruebasws.utils;
+
+import java.util.Set;
+import java.util.ArrayList;
+
+import com.mng.robotest.test80.ParamsBean;
+
+@SuppressWarnings("javadoc")
+public class TSuiteThreadsManager {
+
+    public static final String prefixIdThread = "/Test/";
+
+    public static String getLocatorThreadGroupTestSuite(String suiteName, String channel) {
+        return (
+            prefixIdThread +
+            suiteName + ":" +
+            channel
+        );        
+    }
+    
+    public static String getLocatorThreadTestSuite(ParamsBean paramsTSuite) {
+        return (getLocatorThreadTestSuite(paramsTSuite.getSuiteName(), paramsTSuite.getAppE().toString(), paramsTSuite.getChannel().toString(), paramsTSuite.getBrowser(), paramsTSuite.getVersion(), paramsTSuite.getIdSuiteExecution()));   
+    }
+    
+    public static String getLocatorThreadTestSuite(String suiteName, String application, String channel, String browser, String version, String idSuiteExecution) {
+        return (
+            prefixIdThread +
+            suiteName + ":" +
+            channel + ":" +
+            application + ":" +            
+            browser + ":" + 
+            version + ":" +
+            idSuiteExecution 
+        );
+    }
+
+    public static String getIdExecSuiteFromLocator(String locatorThreadTestSuite) {
+        int iniId = locatorThreadTestSuite.lastIndexOf(":");
+        return (locatorThreadTestSuite.substring(iniId + 1));
+    }
+    
+    public static ArrayList<Thread> getThreadsTestSuiteGroup() {
+        return (getThreadsTestSuiteGroup("", ""));
+    }
+	
+    public static ArrayList<Thread> getThreadsTestSuiteGroup(String suiteName, String channel) { 
+        Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+        ArrayList<Thread> threadsTest = new ArrayList<>();
+        for (Thread thread : threadSet) {
+            if (thread.getName().contains(getLocatorThreadGroupTestSuite(suiteName, channel))) 
+                threadsTest.add(thread);
+        }
+		
+        return threadsTest;
+    }
+    
+    //Interrumpe todos los threads creados en la ejecución de una TestSuite concreta
+    public static ArrayList<Thread> stopThreadsTestSuite(String locatorThreadTestSuite) throws Exception {
+        boolean existThreads = true;
+        ArrayList<Thread> threadsTest = new ArrayList<>();
+        int i=0;
+        while (existThreads && i<=20) {
+            i+=1;
+            existThreads = false;
+            Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
+            for (Thread thread : threadSet) {
+                String threadGroup = "";
+                if (thread.getThreadGroup()!=null) 
+                    threadGroup = thread.getThreadGroup().getName();
+				
+                //Borraremos todos los threads pertenecientes al grupo del thread padre (el que se arranca en execTest.jsp donde se arranca TestNG)
+                //Es decir, el thread padre + todos los threads que se han creado desde el thread padre
+                if (threadGroup.contains(locatorThreadTestSuite)) {
+                    existThreads = true;
+                    threadsTest.add(thread);
+                    thread.interrupt();
+                }
+            }
+			
+            //Este mecanismo tiene un problema y es que por alguna razón en ocasiones se crean nuevos Threads hijos de TestNG con lo que el test no finaliza
+            //Así que esperamos 1 segundo y volvemos a revisar si se han creado nuevos Threads
+            if (existThreads) 
+                Thread.sleep(1000);
+        }
+		
+        return threadsTest;
+    }	
+}
