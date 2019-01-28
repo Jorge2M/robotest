fbf41b2d0729c6ee2a896cec4a2220f31a776a1e
oriol.munoz <oriol.munoz.sge@mango.com>
2019-01-28 12:20:41 +0100

    SYS-3913

--- test80/src/main/java/com/mng/robotest/test80/mango/test/utils/testab/TestABid.java
+++ test80/src/main/java/com/mng/robotest/test80/mango/test/utils/testab/TestABid.java
@@ -46,7 +46,7 @@ public enum TestABid {
 	),
 	GaleriaDesktopReact(
 		TypeTestAB.GoogleExperiments,
-		"sub_8rhk5o_wc6i3wg92ap",
+		"lTzroOP0TgOP5Q9bUV9rLw",
 		"sub_8rhk5o_wc6i3wg92ap",
 		Arrays.asList(0,1),
 		Arrays.asList(
