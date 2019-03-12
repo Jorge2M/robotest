5a49044d005cb78d42b82f3fe660b46aea5f97a6
Jorge Munoz <jorge.munoz.sge@mango.com>
2019-03-12 10:06:17 +0100

    SYS-4150

---
+++ test80/src/main/java/com/mng/robotest/test80/mango/test/pageobject/shop/modales/ModalClubMangoLikes.java
@@ -0,0 +1,31 @@
+package com.mng.robotest.test80.mango.test.pageobject.shop.modales;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.mng.robotest.test80.mango.test.pageobject.WebdrvWrapp;
+
+public class ModalClubMangoLikes {
+
+	private static final String xpathModal = "//div[@class='superfan-modal']/div[@class='modal-container']";
+	private static final String xpathDescubreTusVentajas = xpathModal + "//a";
+	private static final String xpathLinkForClose = xpathModal + "//span[@class='modal-close-icon']";
+	
+	public static boolean isVisible(WebDriver driver) {
+		return WebdrvWrapp.isElementVisible(driver, By.xpath(xpathModal));
+	}
+	
+	public static void clickDescubreTusVentajas(WebDriver driver) throws Exception {
+		WebdrvWrapp.clickAndWaitLoad(driver, By.xpath(xpathDescubreTusVentajas));
+	}
+	
+	public static void closeModalIfVisible(WebDriver driver) throws Exception {
+		if (isVisible(driver)) {
+			closeModal(driver);
+		}
+	}
+	
+	private static void closeModal(WebDriver driver) throws Exception {
+		WebdrvWrapp.clickAndWaitLoad(driver, By.xpath(xpathLinkForClose));
+	}
+}
---
+++ test80/src/main/java/com/mng/robotest/test80/mango/test/stpv/shop/checkout/assistqiwi/PageQiwiConfirmStpV.java
@@ -0,0 +1,16 @@
+package com.mng.robotest.test80.mango.test.stpv.shop.checkout.assistqiwi;
+
+import org.openqa.selenium.WebDriver;
+
+import com.mng.robotest.test80.arq.annotations.step.Step;
+import com.mng.robotest.test80.mango.test.pageobject.shop.checkout.assistqiwi.PageQiwiConfirm;
+
+public class PageQiwiConfirmStpV {
+
+	@Step (
+		description="Seleccionar el botón \"Confirmar\" de la página de confirmación de Qiwi", 
+        expected="Aparece la página de resultado del pago de Mango")
+	public static void selectConfirmButton(WebDriver driver) throws Exception {
+		PageQiwiConfirm.clickConfirmar(driver);         
+    }
+}
