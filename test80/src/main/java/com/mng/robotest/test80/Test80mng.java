65430814d7620d236b74c95ec7b45da0e88de169
Jorge Munoz <jorge.munoz.sge@mango.com>
2019-11-06 16:19:22 +0100

    SYS-5010

--- test80/src/main/java/com/mng/robotest/test80/Test80mng.java
+++ test80/src/main/java/com/mng/robotest/test80/Test80mng.java
@@ -1,6 +1,6 @@
 package com.mng.robotest.test80;
 
-import java.net.HttpURLConnection;
+import java.net.HttpURLConnection;       
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
