bf124ac8763434f40fa40cef6103b02005ce9483
Jorge Munoz <jorge.munoz.sge@mango.com>
2021-09-28 09:41:01 +0200

    #Pipeline

---
+++ test80/src/integrationTests/java/com/mng/robotest/test80/PruebaIT.java
@@ -0,0 +1,16 @@
+package com.mng.robotest.test80;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class PruebaIT {
+
+	@Test
+	public void prueba_aaa() {
+		assertTrue(true);
+	}
+	
+	
+}
