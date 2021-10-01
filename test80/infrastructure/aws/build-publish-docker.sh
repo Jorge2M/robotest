2105f9d917dfdcc8432ab5a40ff2a53baaa2eb63
Jorge Munoz <jorge.munoz.sge@mango.com>
2021-10-01 22:02:59 +0200

    #Publish

--- test80/infrastructure/aws/build-publish-docker.sh
+++ test80/infrastructure/aws/build-publish-docker.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 IMAGE="robotest-mango"
-TAG=$APP_VERSION
 PRO_ACCOUNT_ID="495248209902"
 REPOSITORY_NAME="kamikazes"
 REPOSITORY="$PRO_ACCOUNT_ID.dkr.ecr.eu-west-1.amazonaws.com/mango/$REPOSITORY_NAME/$IMAGE"
 
 docker build --build-arg app_version=$APP_VERSION --tag $IMAGE:latest test80/.
-docker tag "$REPOSITORY_NAME/$IMAGE:latest" "$REPOSITORY:$TAG"
 docker tag "$REPOSITORY_NAME/$IMAGE:latest" "$REPOSITORY:latest"
 
 echo $(aws --version)
