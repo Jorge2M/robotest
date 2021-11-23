7327ba095b272391f431f92a2ddb759d51256d2d
Jorge Munoz <jorge.munoz.sge@mango.com>
2021-11-23 14:22:50 +0100

    #KKC-214

--- test80/docker-entrypoint.sh
+++ test80/docker-entrypoint.sh
@@ -10,4 +10,4 @@ echo "none /dev/shm tmpfs defaults,size=2g 0 0" >> /etc/fstab
 mount -o remount /dev/shm
 echo $SCREEN_RESOLUTION
 echo $URL_CLUSTER
-xvfb-run -s "-screen 0 ${SCREEN_RESOLUTION}" java -cp test80.jar com.mng.robotest.test80.access.rest.ServerRest -port 80 -secureport 443
+xvfb-run -s "-screen 0 ${SCREEN_RESOLUTION}" java -cp test80.jar com.mng.robotest.test80.access.rest.ServerRest -port 8080 -secureport 443
