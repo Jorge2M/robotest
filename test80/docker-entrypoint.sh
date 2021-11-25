ee0bd1dea5d10715afa4e395886696abd845c3ff
Jorge Munoz <jorge.munoz.sge@mango.com>
2021-11-25 10:30:11 +0100

    #KKC-214

--- test80/docker-entrypoint.sh
+++ test80/docker-entrypoint.sh
@@ -4,10 +4,23 @@
 #$1 -> screen resolution (f.e. 1920x1080x24)
 #$2 -> url cluster slaves
 SCREEN_RESOLUTION=$1
-URL_CLUSTER=$2
+
+if [ -z "$URL_HUB" ]; then
+    PARAM_URL_HUB=""
+else 
+    PARAM_URL_HUB="-urlhub $URL_HUB"
+fi
+
+if [ -z "$URL_SLAVES" ]; then
+    PARAM_URL_SLAVES=""
+else 
+    PARAM_URL_SLAVES="-urlslave $URL_SLAVES"
+fi
 
 echo "none /dev/shm tmpfs defaults,size=2g 0 0" >> /etc/fstab
 mount -o remount /dev/shm
+
 echo $SCREEN_RESOLUTION
-echo $URL_CLUSTER
-xvfb-run -s "-screen 0 ${SCREEN_RESOLUTION}" java -cp test80.jar com.mng.robotest.test80.access.rest.ServerRest -port 8080 -secureport 443
+echo $PARAM_URL_HUB
+echo $PARAM_URL_SLAVES
+xvfb-run -s "-screen 0 ${SCREEN_RESOLUTION}" java -cp test80.jar com.mng.robotest.test80.access.rest.ServerRest -port 8080 -secureport 443 ${PARAM_URL_HUB} ${PARAM_URL_SLAVES}
