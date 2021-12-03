6b8f8be9146e155e39f784c49dfdd47579da0574
Jorge Munoz <jorge.munoz.sge@mango.com>
2021-12-03 09:37:51 +0100

    #url_init_slave param

--- test80/docker-entrypoint.sh
+++ test80/docker-entrypoint.sh
@@ -11,9 +11,15 @@ else
     PARAM_URL_HUB="-urlhub $URL_HUB"
 fi
 
+if [ -z "$URL_INIT_SLAVE" ]; then
+    PARAM_URL_INIT_SLAVE=""
+else 
+    PARAM_URL_INIT_SLAVE="-urlinislave $URL_INIT_SLAVE"
+fi
+
 echo "none /dev/shm tmpfs defaults,size=2g 0 0" >> /etc/fstab
 mount -o remount /dev/shm
 
 echo $SCREEN_RESOLUTION
 echo $PARAM_URL_HUB
-xvfb-run -s "-screen 0 ${SCREEN_RESOLUTION}" java -cp test80.jar com.mng.robotest.test80.access.rest.ServerRest -port 8080 -secureport 443 ${PARAM_URL_HUB}
+xvfb-run -s "-screen 0 ${SCREEN_RESOLUTION}" java -cp test80.jar com.mng.robotest.test80.access.rest.ServerRest -port 8080 -secureport 443 ${PARAM_URL_HUB} ${PARAM_URL_INIT_SLAVE}
