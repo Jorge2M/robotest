6e435be06157c0339c2f2d22abe9546d7fa0f3d9
Jorge Munoz <jorge.munoz.sge@mango.com>
2022-01-28 08:54:03 +0100

    #Cambio bolsa outlet-desktop a la nueva bolsa de Shop

--- test80/src/main/java/com/mng/robotest/test80/mango/test/pageobject/shop/bolsa/SecBolsaDesktopOld.java
+++ test80/src/main/java/com/mng/robotest/test80/mango/test/pageobject/shop/bolsa/SecBolsaDesktopOld.java
@@ -5,39 +5,39 @@ import org.openqa.selenium.WebDriver;
 import com.github.jorge2m.testmaker.conf.Channel;
 import com.mng.robotest.test80.mango.conftestmaker.AppEcom;
 
-
-public class SecBolsaDesktopOld extends SecBolsaDesktop {
+//TODO remove
+public class SecBolsaDesktopOld /*extends SecBolsaDesktop*/ {
 	
-	private final LineasArtBolsa lineasArtBolsa;
-	
-	private static final String XPathPanelBolsa = "//div[(@class='shoppingBagContent' or @id='shoppingBagContent') and not(@style[contains(.,'display: none')])]//div[@id='mainDivBolsa']"; 
-	private static final String XPathBotonComprar = XPathPanelBolsa + "//*[@id='bolsaComprar']";
-	
-	private static final String XPathPrecioSubTotal = "//*[@class='box_total_price']";
-	
-	public SecBolsaDesktopOld(Channel channel, AppEcom app, WebDriver driver) {
-		super(channel, app, driver);
-		lineasArtBolsa = new LineasArtBolsaDesktopOld(channel, driver);
-	}
-	
-	@Override
-	String getXPathPanelBolsa() {
-		return XPathPanelBolsa;
-	}
-	
-	@Override
-	String getXPathBotonComprar() {
-		return XPathBotonComprar; 
-	}
-	
-	@Override
-	String getXPathPrecioSubTotal() {
-		return XPathPrecioSubTotal; 
-	}	  
-	
-	@Override
-	public LineasArtBolsa getLineasArtBolsa() {
-		return lineasArtBolsa;
-	}
+//	private final LineasArtBolsa lineasArtBolsa;
+//	
+//	private static final String XPathPanelBolsa = "//div[(@class='shoppingBagContent' or @id='shoppingBagContent') and not(@style[contains(.,'display: none')])]//div[@id='mainDivBolsa']"; 
+//	private static final String XPathBotonComprar = XPathPanelBolsa + "//*[@id='bolsaComprar']";
+//	
+//	private static final String XPathPrecioSubTotal = "//*[@class='box_total_price']";
+//	
+//	public SecBolsaDesktopOld(Channel channel, AppEcom app, WebDriver driver) {
+//		super(channel, app, driver);
+//		lineasArtBolsa = new LineasArtBolsaDesktopOld(channel, driver);
+//	}
+//	
+//	@Override
+//	String getXPathPanelBolsa() {
+//		return XPathPanelBolsa;
+//	}
+//	
+//	@Override
+//	String getXPathBotonComprar() {
+//		return XPathBotonComprar; 
+//	}
+//	
+//	@Override
+//	String getXPathPrecioSubTotal() {
+//		return XPathPrecioSubTotal; 
+//	}	  
+//	
+//	@Override
+//	public LineasArtBolsa getLineasArtBolsa() {
+//		return lineasArtBolsa;
+//	}
 
 }
