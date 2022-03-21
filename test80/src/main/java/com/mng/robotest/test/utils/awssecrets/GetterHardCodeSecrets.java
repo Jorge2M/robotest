14e3523e8a7e2d3e456a0e42a797cf06517ee447
Jorge Munoz <jorge.munoz.sge@mango.com>
2022-03-21 08:47:55 +0100

    #Ajustes

--- test80/src/main/java/com/mng/robotest/test/utils/awssecrets/GetterHardCodeSecrets.java
+++ test80/src/main/java/com/mng/robotest/test/utils/awssecrets/GetterHardCodeSecrets.java
@@ -28,7 +28,7 @@ public class GetterHardCodeSecrets extends GetterSecretsFromInputStream {
 			"	</secret>" + lineJump +
 			"	<secret type=\"MANTO_USER\">" + lineJump +
 			"		<user>00556106</user>" + lineJump +
-			"		<password>Ireneloca2016</password>" + lineJump +
+			"		<password>Ireneloca2017</password>" + lineJump +
 			"	</secret>" + lineJump +
 			"	<secret type=\"BROWSERSTACK_USER\">" + lineJump +
 			"		<user>equipoqa1</user>" + lineJump +
