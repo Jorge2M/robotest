a87baec33b369007d3623e6ceeab49058770025b
JORGE MUÑOZ MARTINEZ <jorge.munoz.sge@mango.com>
2022-06-01 09:35:15 +0200

    Ajuste get Cookies

--- src/test/java/com/mng/robotest/domains/cookiescheck/services/CookiesFinderIT.java
+++ src/test/java/com/mng/robotest/domains/cookiescheck/services/CookiesFinderIT.java
@@ -3,20 +3,20 @@ package com.mng.robotest.domains.cookiescheck.services;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 
 import com.mng.robotest.domains.cookiescheck.entities.Cookie;
-import com.mng.robotest.domains.cookiescheck.services.CookiesFinder;
 
 public class CookiesFinderIT {
 
 	@Test
 	public void testGetCookies() throws Exception {
 		CookiesFinder cookieFinder = new CookiesFinder();
-		List<Cookie> cookies = cookieFinder.getAllowedCookies();
+		Optional<List<Cookie>> cookies = cookieFinder.getAllowedCookies();
 		
-		assertTrue(cookies!=null && cookies.size()>0);
+		assertTrue(cookies.isPresent() && cookies.get().size()>0);
 		assertTrue(!cookieFinder.isNeededRefreshDataCookies(5));
 		Thread.sleep(1001);
 		assertTrue(cookieFinder.isNeededRefreshDataCookies(1));
