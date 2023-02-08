beccce7aa3bc1a90c00fe4803f92913020656239
JORGE MUÑOZ MARTINEZ <jorge.munoz.sge@mango.com>
2023-02-08 16:34:28 +0100

    PIUR-4401

--- src/main/java/com/mng/robotest/domains/compra/tests/Compra.java
+++ src/main/java/com/mng/robotest/domains/compra/tests/Compra.java
@@ -37,7 +37,7 @@ public class Compra {
 	}
 	
 	@Test (
-		groups={"Compra", "Canal:all_App:all"}, alwaysRun=true,
+		groups={"Registro_Express", "Canal:all_App:all"}, alwaysRun=true,
 		description="[Usuario no registrado] Pre-compra. Cierre/Inicio sesión correcto")
 	public void COM005_Compra_noReg_emailNoExist() throws Exception {
 		new Com005().execute();
