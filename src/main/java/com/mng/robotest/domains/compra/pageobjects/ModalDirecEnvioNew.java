c050d89d1902464332798119529cd13577e39ad1
JORGE MUÑOZ MARTINEZ <jorge.munoz.sge@mango.com>
2023-03-28 17:12:33 +0200

    PIUR-4553 Ajustar a nuevo modal de direcciones

--- src/main/java/com/mng/robotest/domains/compra/pageobjects/ModalDirecEnvioNew.java
+++ src/main/java/com/mng/robotest/domains/compra/pageobjects/ModalDirecEnvioNew.java
@@ -8,29 +8,54 @@ import static com.github.jorge2m.testmaker.service.webdriver.pageobject.StateEle
 public class ModalDirecEnvioNew extends PageBase {
 
 	private static final String XPATH_CHECK_DIRECION_PRINCIPAL = "//*[@for='address.form-isMainAddress']";
+	
+	//TODO dejar sólo una versión cuando la operativa de PRE suba a PRO (28-marzo-2023)
 	private static final String XPATH_SAVE_BUTTON = "//*[@data-testid[contains(.,'save.button')]]";
+	private static final String XPATH_SAVE_BUTTON_PRE = "//*[@data-testid='deliveryAddress.form.button.submit']";
+	
 	private static final String XPATH_REMOVE_BUTTON = "//*[@data-testid='address.form.delete.button']";
+	private static final String XPATH_REMOVE_BUTTON_PRE = "//*[@data-testid='deliveryAddress.form.button.delete']";
+	
 	private static final String XPATH_REMOVE_CONFIRM_BUTTON = "//button[@data-testid='address.form.modal.delete.button']";
 	private static final String XPATH_SELECTOR_PROVINCIA = "//select[@id='address.form.provinceId']";
+	private static final String XPATH_PROVINCIA_OPTIONS_PRE = "//div[@data-testid='addressForm.provinceId']";
+	private static final String XPATH_PROVINCIA_SELECTED_PRE = XPATH_PROVINCIA_OPTIONS_PRE + "//div[@aria-selected='true']";
+	private static final String XPATH_PROVINCIA_SELECT = "//label[@for='provinceId']";
 
 	public enum InputType {
-		NOMBRE("address.form.firstName"),
-		APELLIDOS("address.form.lastName"),
-		DIRECCION("address.form.address"),
-		CODIGO_POSTAL("address.form.postalCode"),
-		MOVIL("address.form.phoneNumber"),
-		CITY("address.form.city"),
-		PROVINCIA("address.form.provinceName");
+		NOMBRE("address.form.firstName", "addressForm.firstName"),
+		APELLIDOS("address.form.lastName", "addressForm.lastName"),
+		DIRECCION("address.form.address", "addressForm.address"),
+		CODIGO_POSTAL("address.form.postalCode", "addressForm.postalCode"),
+		MOVIL("address.form.phoneNumber", "address.form.number"),
+		CITY("address.form.city", "addressForm.city"),
+		PROVINCIA("address.form.provinceName", "addressForm.provinceName");
 
 		private String dataTestId;
-		private InputType(String dataTestId) {
+		private String dataTestIdPre;
+		private InputType(String dataTestId, String dataTestIdPre) {
 			this.dataTestId = dataTestId;
+			this.dataTestIdPre = dataTestIdPre;
 		}
 		public String getXPath() {
-			return String.format("//input[@data-testid='%s']", dataTestId);
+			return String.format("//input[@data-testid='%s' or @data-testid='%s']", dataTestId, dataTestIdPre);
 		}
-
 	}
+	
+	private String getXPathSaveButton() {
+		return "(" + XPATH_SAVE_BUTTON + " | " + XPATH_SAVE_BUTTON_PRE + ")";
+	}
+	
+	
+	private String getXPathRemoveButton() {
+		return "(" + XPATH_REMOVE_BUTTON + " | " + XPATH_REMOVE_BUTTON_PRE + ")";
+	}
+	
+	private String getXPathSelectorProvincia() {
+		return "(" + XPATH_SELECTOR_PROVINCIA + " | " + XPATH_PROVINCIA_OPTIONS_PRE + ")";
+	}
+	
+	
 	public boolean isVisible(int seconds) {
 		return state(Present, InputType.NOMBRE.getXPath()).wait(seconds).check();
 	}
@@ -79,15 +104,34 @@ public class ModalDirecEnvioNew extends PageBase {
 	}
 	
 	private void setProvinciaIfNotYet() {
-		if (state(Visible, XPATH_SELECTOR_PROVINCIA).check()) {
-			String valueInicial = getElement(XPATH_SELECTOR_PROVINCIA).getAttribute("value");
-			if ("".compareTo(valueInicial)==0) {
+		if (state(Present, getXPathSelectorProvincia()).check()) {
+			if ("".compareTo(getValueInitialProvincia())==0) {
 				setProvincia();
 			}
 		}
 	}
 
+	private String getValueInitialProvincia() {
+		if (state(Visible, XPATH_SELECTOR_PROVINCIA).check()) {
+		    return getElement(XPATH_SELECTOR_PROVINCIA).getAttribute("value");
+		}
+		return getElement(XPATH_PROVINCIA_SELECTED_PRE).getText();
+	}
+	
 	private void setProvincia() {
+		if (state(Visible, XPATH_SELECTOR_PROVINCIA).check()) {
+			setProvinciaPro();
+		} else {
+			setProvinciaPre();
+		}
+	}
+	
+	private void setProvinciaPre() {
+		click(XPATH_PROVINCIA_SELECT).exec();
+		click(XPATH_PROVINCIA_OPTIONS_PRE + "/div").exec();
+	}
+	
+	private void setProvinciaPro() {
 		//workaround because doesn't run the Select sentence (seems a Selenium Bug)
 		String allCharacters = "abcdefghijklmnopqrstuvwxyz";
 		for (int i=0; i<allCharacters.length(); i++) {
@@ -107,11 +151,11 @@ public class ModalDirecEnvioNew extends PageBase {
 	}
 	
 	public void clickSaveButton() {
-		click(XPATH_SAVE_BUTTON).exec();
+		click(getXPathSaveButton()).exec();
 	}
 	
 	public void clickRemoveButton() {
-		click(XPATH_REMOVE_BUTTON).exec();
+		click(getXPathRemoveButton()).exec();
 	}
 
 	public boolean isVisibleModalConfirmacionEliminar(int seconds) {
