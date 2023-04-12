22ef41659a45be2fcde096dc1ace5b759ebcd2c5
JORGE MUÑOZ MARTINEZ <jorge.munoz.sge@mango.com>
2023-04-12 16:52:26 +0200

    Ajuste Robotest

--- src/main/java/com/mng/robotest/test/pageobject/shop/menus/desktop/ModalUserSesionShopDesktop.java
+++ src/main/java/com/mng/robotest/test/pageobject/shop/menus/desktop/ModalUserSesionShopDesktop.java
@@ -20,7 +20,7 @@ public class ModalUserSesionShopDesktop extends PageBase {
 		INICIAR_SESION (XPATH_CAPA_MENUS + "//div[@class[contains(.,'login-button')]]"),
 		
 		//Pedir React ID
-		REGISTRATE (XPATH_CAPA_MENUS + "//span[@class='login-register-link' or @class[contains(.,'register-link')] or text()='Regístrate' or @class[continas(.,'RuqlF')]]"),
+		REGISTRATE (XPATH_CAPA_MENUS + "//span[@class='login-register-link' or @class[contains(.,'register-link')] or text()='Regístrate' or @class[contains(.,'RuqlF')]]"),
 		MI_CUENTA (XPATH_CAPA_MENUS + "//div[@data-testid[contains(.,'mi_cuenta')]]"),
 		MIS_COMPRAS (XPATH_CAPA_MENUS + "//div[@data-testid[contains(.,'mis_compras')]]"),	 
 		MANGO_LIKES_YOU (XPATH_CAPA_MENUS + "//div[@data-testid[contains(.,'mango_likes_you')]]"),
