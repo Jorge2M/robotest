1087bc5367b975b40fbfe1e362bcc503fec1d75c
JORGE MUÑOZ MARTINEZ <jorge.munoz.sge@mango.com>
2023-07-21 12:00:57 +0200

    Ajustar FIC001

--- src/main/java/com/mng/robotest/repository/productlist/filter/FilterBlackList.java
+++ src/main/java/com/mng/robotest/repository/productlist/filter/FilterBlackList.java
@@ -1,16 +1,16 @@
 package com.mng.robotest.repository.productlist.filter;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.mng.robotest.repository.productlist.entity.GarmentCatalog;
 
 public class FilterBlackList implements Filter {
 	
-//	private static final List<String> BLACK_LIST = Arrays.asList(
-//		"57029404" //Al intentar darlo de alta en la bolsa aparece un error de artículo agotado
-//	);
-	private static final List<String> BLACK_LIST = new ArrayList<>();
+	private static final List<String> BLACK_LIST = Arrays.asList(
+		"47040356" //El artículo está casi agotado en todas las tallas
+	);
+//	private static final List<String> BLACK_LIST = new ArrayList<>();
 	
 	@Override
 	public List<GarmentCatalog> filter(List<GarmentCatalog> garments) throws Exception {
