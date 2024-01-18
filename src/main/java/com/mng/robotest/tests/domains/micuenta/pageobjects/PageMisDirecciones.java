ecb683e699f646fa04e31479baa4346ece3f47f9
JORGE MUÑOZ MARTINEZ <jorge.munoz.sge@mango.com>
2024-01-18 12:18:07 +0100

    ONLINE-49869

--- src/main/java/com/mng/robotest/tests/domains/micuenta/pageobjects/PageMisDirecciones.java
+++ src/main/java/com/mng/robotest/tests/domains/micuenta/pageobjects/PageMisDirecciones.java
@@ -9,9 +9,8 @@ public class PageMisDirecciones extends PageBase {
 	private static final String XP_MICROFRONTEND = "//micro-frontend[@id='myAddresses']"; 
 	private static final String XP_LINK_EDITAR = "//*[@data-testid[contains(.,'addressCard')]]//a";
 	private static final String XP_INPUT_CODPOSTAL = "//input[@data-testid[contains(.,'.postalCode')]]";
-	private static final String XP_POBLACION_DESPLEGABLE_DESKTOP = "//div[@aria-labelledby='city.label']//p";
-	private static final String XP_INPUT_POBLACION_DESKTOP = "//input[@data-testid='addressForm.city']";
-	private static final String XP_INPUT_POBLACION_DEVICE = "//select[@id='city']";
+	private static final String XP_POBLACION_DESPLEGABLE = "//div[@aria-labelledby='city.label']//p";
+	private static final String XP_INPUT_POBLACION = "//input[@data-testid='addressForm.city']";
 	private static final String XP_INPUT_DIRECCION = "//input[@data-testid[contains(.,'.address')]]";
 	private static final String XP_BOTON_GUARDAR = "//*[@data-testid='deliveryAddress.form.button.submit']";
 	
@@ -35,23 +34,13 @@ public class PageMisDirecciones extends PageBase {
 	}
 
 	public String getPoblacion() {
-		if (channel.isDevice()) {
-			return getPoblacionDevice();
-		}
-		return getPoblacionDesktop();
-	}	
-	private String getPoblacionDevice() {
-		var inputPoblacion = getElement(XP_INPUT_POBLACION_DEVICE);
-		return inputPoblacion.getAttribute("value");
-	}
-	private String getPoblacionDesktop() {
-		if (state(VISIBLE, XP_POBLACION_DESPLEGABLE_DESKTOP).check()) {
-			var inputPoblacion = getElement(XP_POBLACION_DESPLEGABLE_DESKTOP);
+		if (state(VISIBLE, XP_POBLACION_DESPLEGABLE).check()) {
+			var inputPoblacion = getElement(XP_POBLACION_DESPLEGABLE);
 			return inputPoblacion.getText();
 		}
-		var inputPoblacion = getElement(XP_INPUT_POBLACION_DESKTOP);
+		var inputPoblacion = getElement(XP_INPUT_POBLACION);
 		return inputPoblacion.getAttribute("value");
-	}
+	}	
 	
 	public String getDireccion() {
 		return getElement(XP_INPUT_DIRECCION).getAttribute("value");
