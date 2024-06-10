e6eb7258d86dd8aeff87b95086ffe44fca8c7f36
JORGE MUÑOZ MARTINEZ <jorge.munoz.sge@mango.com>
2024-06-10 13:30:18 +0200

    update image 20240610103700-fd0a73e293

--- src/main/java/com/mng/robotest/tests/domains/menus/steps/MenuSteps.java
+++ src/main/java/com/mng/robotest/tests/domains/menus/steps/MenuSteps.java
@@ -38,7 +38,7 @@ import static com.mng.robotest.tests.domains.menus.beans.FactoryMenus.MenuItem.A
 
 public class MenuSteps extends StepBase {
 
-	private static final List<String> MENUS_WITHOUT_ARTICLES = Arrays.asList("Essentials", "Antoine Griezmann", "Trajes", "CAPSULE");
+	private static final List<String> MENUS_WITHOUT_ARTICLES = Arrays.asList("Essentials", "Antoine Griezmann", "Trajes", "CAPSULE", "Fiesta y eventos");
 	
 	@Override
 	public void clickMenu(String menuLabel) {
