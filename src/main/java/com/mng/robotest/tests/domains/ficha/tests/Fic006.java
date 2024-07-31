94bef5a357eb2a73f4e51270151db40ad7d8e269
JORGE MUÑOZ MARTINEZ <jorge.munoz.sge@mango.com>
2024-07-31 09:40:11 +0200

    Ajustes

--- src/main/java/com/mng/robotest/tests/domains/ficha/tests/Fic006.java
+++ src/main/java/com/mng/robotest/tests/domains/ficha/tests/Fic006.java
@@ -57,7 +57,9 @@ public class Fic006 extends TestBase {
 	
 	private void accessAndClear() throws Exception {
 		accessAndClearData();
-		PageFavoritos.make(dataTest.getPais(), app).clearAllArticulos();
+		if (!isOutlet()) {
+			PageFavoritos.make(dataTest.getPais(), app).clearAllArticulos();
+		}
 	}
 
 	private void articleOnlineTest() {
