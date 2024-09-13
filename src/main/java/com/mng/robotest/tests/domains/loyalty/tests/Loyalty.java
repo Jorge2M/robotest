02776fd18594cbfa8699598c0d6d8b9402688685
JORGE MUÑOZ MARTINEZ <jorge.munoz.sge@mango.com>
2024-09-13 07:34:45 +0200

    Ajustes

--- src/main/java/com/mng/robotest/tests/domains/loyalty/tests/Loyalty.java
+++ src/main/java/com/mng/robotest/tests/domains/loyalty/tests/Loyalty.java
@@ -19,7 +19,8 @@ import com.mng.robotest.tests.domains.loyalty.tests.testsnew.Loy007;
 public class Loyalty {
 	
 	@Test (
-		testName="LOY001",	
+		testName="LOY001",
+		enabled=false,		
 		groups={"Loyalty", "Smoke", "Compra", "Checkout", "Canal:desktop,mobile_App:shop"},
 		description="Se realiza una compra con envío a tienda OK comprobando que se aplican correctamente los Likes")
 	public void compraLikesStored() throws Exception {
@@ -28,7 +29,6 @@ public class Loyalty {
 	
 	@Test (
 		testName="LOY901",
-		enabled=false,
 		groups={"Loyalty", "Smoke", "Compra", "Checkout", "Canal:desktop,mobile_App:shop"},
 		description="[USA][Legacy Loyalty] Se realiza una compra con envío a tienda OK comprobando que se aplican correctamente los Likes")
 	public void compraLikesStoredLegacy() throws Exception {
@@ -36,7 +36,8 @@ public class Loyalty {
 	}	
 	
 	@Test (
-		testName="LOY002",			
+		testName="LOY002",
+		enabled=false,		
 		groups={"Loyalty", "Canal:desktop,mobile_App:shop"},
 		description="Exchange mediante donación de Likes")
 	public void exhangeDonacionLikes() throws Exception {
@@ -45,7 +46,6 @@ public class Loyalty {
 	
 	@Test (
 		testName="LOY902",
-		enabled=false,
 		groups={"Loyalty", "Canal:desktop,mobile_App:shop"},
 		description="[USA][Legacy Loyalty] Exchange mediante donación de Likes")
 	public void exhangeDonacionLikesLegacy() throws Exception {
@@ -53,7 +53,8 @@ public class Loyalty {
 	}	
 
 	@Test (
-		testName="LOY003",			
+		testName="LOY003",
+		enabled=false,		
 		groups={"Loyalty", "Smoke", "Canal:desktop,mobile_App:shop"},
 		description="Compra entrada cine a cambio de Likes")
 	public void exhangeCompraEntrada() throws Exception {
@@ -62,7 +63,6 @@ public class Loyalty {
 	
 	@Test (
 		testName="LOY903",
-		enabled=false,
 		groups={"Loyalty", "Smoke", "Canal:desktop,mobile_App:shop"},
 		description="[USA][Legacy Loyalty] Compra entrada cine a cambio de Likes")
 	public void exhangeCompraEntradaLegacy() throws Exception {
@@ -70,7 +70,8 @@ public class Loyalty {
 	}	
 	
 	@Test (
-		testName="LOY004",			
+		testName="LOY004",
+		enabled=false,		
 		groups={"Loyalty", "Canal:desktop,mobile_App:shop"},
 		description="Navegación por Mango Likes You History y Help")
 	public void historyHelp() throws Exception {
@@ -79,7 +80,6 @@ public class Loyalty {
 	
 	@Test (
 		testName="LOY904",
-		enabled=false,
 		groups={"Loyalty", "Canal:desktop,mobile_App:shop"},
 		description="[USA][Legacy Loyalty] Navegación por Mango Likes You History y Help")
 	public void historyHelpLegacy() throws Exception {
@@ -87,7 +87,8 @@ public class Loyalty {
 	}	
 	
 	@Test (
-		testName="LOY006",			
+		testName="LOY006",
+		enabled=false,		
 		groups={"Loyalty", "Canal:desktop,mobile_App:outlet"},
 		description="Acceso a la Landing y comprobación de que no existen referencias a Loyalty")
 	public void outletWithoutLoyalty() throws Exception {
@@ -96,7 +97,6 @@ public class Loyalty {
 	
 	@Test (
 		testName="LOY906",
-		enabled=false,
 		groups={"Loyalty", "Canal:desktop,mobile_App:outlet"},
 		description="[USA][Legacy Loyalty] Acceso a la Landing y comprobación de que no existen referencias a Loyalty")
 	public void outletWithoutLoyaltyLegacy() throws Exception {
@@ -104,7 +104,8 @@ public class Loyalty {
 	}	
 	
 	@Test (
-		testName="LOY007",			
+		testName="LOY007",
+		enabled=false,		
 		groups={"Loyalty", "Compra", "Checkout", "Canal:desktop,mobile_App:shop"},
 		description="Se realiza un primer pago KO y después uno con envío a domicilio OK comprobando que se aplican correctamente los Likes")
 	public void pagoKoOkLikesStoredLegacy() throws Exception {
@@ -113,7 +114,6 @@ public class Loyalty {
 	
 	@Test (
 		testName="LOY907",			
-		enabled=false,
 		groups={"Loyalty", "Compra", "Checkout", "Canal:desktop,mobile_App:shop"},
 		description="[UK][Legacy Loyalty] Se realiza un primer pago KO y después uno con envío a domicilio OK comprobando que se aplican correctamente los Likes")
 	public void pagoKoOkLikesStored() throws Exception {
